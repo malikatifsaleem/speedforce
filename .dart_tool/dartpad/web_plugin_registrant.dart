fe3d022032647b404a692efd82f70ad967dc24c4
atifawan45 <atifsaleem3101@gmail.com>
2024-08-30 19:43:39 +0500

    update

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -7,16 +7,12 @@
 // ignore_for_file: type=lint
 
 import 'package:libphonenumber_web/libphonenumber_web.dart';
-import 'package:share_plus/src/share_plus_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
-import 'package:url_launcher_web/url_launcher_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
   LibPhoneNumberPlugin.registerWith(registrar);
-  SharePlusWebPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
-  UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
